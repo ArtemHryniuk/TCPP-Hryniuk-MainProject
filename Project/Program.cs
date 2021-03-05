b164e78ad935519842325ad67f5f047a0a64fca3
hryniuk.a <hryniuk.a@donnu.edu.ua>
2021-03-05 20:38:45 +0200

    Змінено з IDE

--- Project/Program.cs
+++ Project/Program.cs
@@ -6,7 +6,7 @@ namespace Project
     {
         private static void Main()
         {
-            // Нова зміна
+            // Змінено з IDE
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             Console.InputEncoding = System.Text.Encoding.UTF8;
 
